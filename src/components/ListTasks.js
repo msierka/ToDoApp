204f3fb4869e4768f4b158116bc313b951af9714
ms <mateusz@>
2023-11-21 18:54:55 +0100

    View tasks in the completed tasks section

--- src/components/ListTasks.js
+++ src/components/ListTasks.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Task from './Task';
  const ListTasks = (props) => {
-    const tasks = props.tasks.map(task => <Task key={task.id} task={task} deleteTask={props.delete} changeTask ={props.change} />)
+    const active = props.tasks.filter(task => task.active)
+    const done = props.tasks.filter(task => !task.active)
+    const activeTasks = active.map(task => <Task key={task.id} task={task} deleteTask={props.delete} changeTask ={props.change} />)
+    const doneTasks = done.map(task => <Task key={task.id} task={task} deleteTask={props.delete} changeTask ={props.change} />)
 
     return ( 
         <>
         <div className='active'>
             <h1>Lista zadań</h1>
-            {tasks}
+            {activeTasks}
             <hr/>
         </div>
         <div className='done'>
-            <h2>Zadania zrobione (0)</h2>
+            <h2>Zadania zrobione ({doneTasks.length})</h2>
+            {doneTasks}
             <hr/>
         </div>
         </>
