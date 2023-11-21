df615951fe8e395997ffba694472b2750e31c0c8
ms <mateusz@>
2023-11-21 19:17:44 +0100

    giving style to important tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
 const Task = (props) => {
-    const {text, date, id} = props.task;
-
+    const {text, date, id,active, priorytet, finishDate} = props.task;
+    const style ={
+        color: 'red'
+    }
+    if(active){
     return (  
         <p>
-        <strong>{text}</strong>- do {date}
+        <strong style={priorytet ? style : null}>{text}</strong>- do {date}
         <button onClick={() => props.changeTask(id)}> Zrobione</button>
         <button onClick={() => props.deleteTask(id)}> Usuń</button>
 
         </p>
         
     );
+    } else {
+        const finish = new Date(finishDate).toLocaleDateString()
+        return (  
+            <p>
+            {text}- do {date} <br/>
+            <em>Wykonano: {finish}</em>
+            </p>
+            
+        );
+    }
 }
  
 export default Task;
